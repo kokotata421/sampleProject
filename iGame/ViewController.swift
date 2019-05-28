a16f1a29345502a445550455d37eb6cb50b2dcd8
Kota <kotakawanishi@KotanoMacBook-Pro.local>
2019-05-28 21:10:10 +0900

    initial commit

---
+++ iGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iGame
+//
+//  Created by Kota Kawanishi on 2019/05/28.
+//  Copyright © 2019 Kota Kawanishi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
